82c03dfc964afb4a0ee1feb850348fca57b425fc
1999JD <writer159.new@gmail.com>
2025-05-15 17:57:33 +0800

    feat: 新增 tag 搜尋

--- src/app/(home)/essential-oil/page.tsx
+++ src/app/(home)/essential-oil/page.tsx
@@ -1,28 +1,40 @@
-import { Box, IconButton, Input, Typography } from "@mui/joy";
+import { Box, Button, Input, Stack, Typography } from "@mui/joy";
 import { Search } from 'lucide-react';
 import RenderList from "./components/RenderLIst";
+import FormControl from '@mui/joy/FormControl';
+import SelectMultipleAppearance from "./components/SelectTags";
 export default function EssentialOilList() {
     return (
         <div className="container mx-auto py-8 px-4 ">
             <Typography level="h1" sx={{ marginBottom: '1rem' }} >Essential Oils</Typography>
-            <Input
-                size="sm"
-                variant="outlined"
-                placeholder="Search keywords..."
-                startDecorator={<Search />}
-                sx={{
-                    alignSelf: 'center',
-                    display: {
-                        xs: 'none',
-                        sm: 'flex',
-                    },
-                    marginBottom: '1rem',
-                    width: '360px'
-                }}
-            />
+            <Box sx={{ marginBottom: 2 }} >
+                <form action="">
+                    <Stack spacing={2} direction={'row'} alignItems={'center'} >
+                        <FormControl>
+                            <Input
+                                variant="outlined"
+                                placeholder="Search keywords..."
+                                startDecorator={<Search />}
+                                sx={{
+                                    alignSelf: 'center',
+                                    display: {
+                                        xs: 'none',
+                                        sm: 'flex',
+                                    },
+                                    width: '360px'
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <SelectMultipleAppearance />
+                        </FormControl>
+                        <Button>送出</Button>
+                    </Stack>
+                </form>
+            </Box>
             <RenderList />
 
 
-        </div>
+        </div >
     );
 }
